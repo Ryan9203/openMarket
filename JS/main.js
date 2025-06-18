aa9a59ec40cec1325e35f422190c47beb2f58b2f
Minsu Kim <rlaalstn6363@gmail.com>
2025-06-18 16:47:38 +0900

    Landing-page의 Product List 데이터 연동

--- JS/main.js
+++ JS/main.js
@@ -10,4 +10,48 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// Product List
+
+document.addEventListener("DOMContentLoaded", () => {
+  const productList = document.querySelector(".product-list");
+
+  // 상품 카드 생성 함수
+  function createProductCard(product) {
+    const card = document.createElement("div");
+    card.classList.add("product-card");
+
+    card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}" />
+      <p class="brand">${product.seller.store_name}</p>
+      <h3 class="product-title">${product.name}</h3>
+      <p class="product-price">${product.price.toLocaleString()}<span>원</span></p>
+    `;
+
+    return card;
+  }
+
+  // 상품 목록 API 호출
+
+  async function fetchProducts() {
+
+    try {
+      const res = await fetch("https://api.wenivops.co.kr/services/open-market/products/");
+      const data = await res.json();
+
+      if (data.count === 0) {
+        productList.innerHTML = `<P>상품이 존재하지 않습니다.</p>`;
+        return;
+      }
+
+      data.results.forEach(product => {
+        const card = createProductCard(product);
+        productList.appendChild(card);
+      });
+    } catch (err) {
+      console.error("상품 목록 불러오기 실패", err);
+      productList.innerHTML = `<P>상품을 불러오는 데 문제가 발생했습니다.</p>`;
+    }
+  }
+  fetchProducts();
+});
 
